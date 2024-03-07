7ab03764203188170af41e2e3bd9d1121ce4144d
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2024-03-07 17:29:05 +0200

    add hook useFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error(`An error occurred: ${response.statusText}`);
+            }
+            const data = await response.json();
+            setData(data);
+            setError(null);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    fetchData();
+    }, [url]);
+
+    return { data, isLoading, error };
+};
+
+export default useFetch;
