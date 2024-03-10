eeb17cd8e818b24b22508f9a3765684655e69bd5
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2024-03-10 14:57:42 +0200

    fix error page

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,27 +4,25 @@ function useFetch(url) {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState({ message: null, code: null });
-
+    
     useEffect(() => {
         setIsLoading(true);
         setData(null);
-        setError({ message: null, code: null });
-
+        setError({ code: null, message: null });
+    
         fetch(url)
-            .then((response) => {
+          .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok', {
-                cause: { code: response.status }
-                });
+              throw Error(response.statusText, { code: response.status });
             }
             return response.json();
-            })
-            .then((respData) => setData(respData))
-            .catch((e) => {
-                setError({ message: e.message, code: e.cause?.code || "Unknown" });
-            })
-            .finally(() => setIsLoading(false));
-    }, [url]);
+          })
+          .then(data => setData(data))
+          .catch(error => {
+            setError({ code: error.code || 'Unknown', message: error.message });
+          })
+          .finally(() => setIsLoading(false));
+      }, [url]);
 
     return [data, isLoading, error];
 }
