b9e90e33846c56971e9348fdf2d3cc871c2717b3
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2024-03-10 15:22:25 +0200

    fix useFetch and laptopRepair

--- src/components/pages/laptopRepair/LaptopRepair.js
+++ src/components/pages/laptopRepair/LaptopRepair.js
@@ -1,11 +1,28 @@
 import React from "react";
 
+import useFetch from "../../../hooks/useFetch";
+import Error from "../error/Error";
+import Loading from "../../loading/Loading";
+
 import "./LaptopRepair.scss";
 
 export default function LaptopRepair() {
+    const { data, isLoading, error } = useFetch(
+        "http://localhost:8000/laptop-repairs"
+    );
+
+    if(isLoading) {
+        return <Loading />;
+    }
+
+    if(error) {
+        return <Error code={error.code} message={error.message} />;
+    }
+
     return (
         <div className="laptop-repair">
             <h1>Ремонт ноутбуків</h1>
+            {data}
         </div>
     );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,27 +4,27 @@ function useFetch(url) {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState({ message: null, code: null });
-    
+
     useEffect(() => {
         setIsLoading(true);
         setData(null);
         setError({ code: null, message: null });
     
         fetch(url)
-          .then(response => {
+            .then(response => {
             if (!response.ok) {
-              throw Error(response.statusText, { code: response.status });
+                throw Error(response.statusText, { code: response.status });
             }
             return response.json();
-          })
-          .then(data => setData(data))
-          .catch(error => {
-            setError({ code: error.code || 'Unknown', message: error.message });
-          })
-          .finally(() => setIsLoading(false));
+            })
+            .then(data => setData(data))
+            .catch(error => {
+                setError({ code: error.code || 'Unknown', message: error.message });
+            })
+            .finally(() => setIsLoading(false));
       }, [url]);
 
-    return [data, isLoading, error];
+    return {data, isLoading, error};
 }
 
 export default useFetch;
