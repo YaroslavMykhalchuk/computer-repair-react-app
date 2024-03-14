ff222e91e5c0b522b25347557c23e0eaf54e919b
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2024-03-14 02:07:45 +0200

    price page and accordion

---
+++ src/components/accordion/Accordion.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Accordion.scss";
+
+const Accordion = (props) => {
+    
+}
--- src/components/pages/price/Price.js
+++ src/components/pages/price/Price.js
@@ -1,9 +1,40 @@
 import React from "react";
 
+import useFetch from "../../../hooks/useFetch";
+import Error from "../error/Error";
+import Loading from "../../loading/Loading";
+
+import "./Price.scss";
+
 export default function Price() {
+    const { data, isLoading, error } = useFetch(
+        "http://localhost:8000/api/prices"
+    );
+
+    if(isLoading) {
+        return <Loading />;
+    }
+
+    if (error && error.message) {
+        return <Error code={error.code} message={error.message} />;
+    }
+
     return (
-        <div>
-            Ціни
+        <div className="price">
+            <h1>Ціни</h1>
+
+            <p className="fs-3 fw-light m-2">
+                Ласкаво просимо на сторінку цін "Тяп-ляп", де прозорість та доступність стоять на першому місці! 
+                Ми розуміємо, наскільки важливо для вас знати вартість послуг заздалегідь, 
+                тому надаємо повний перелік наших цін на ремонт та обслуговування вашої комп'ютерної техніки. 
+                Наша мета – забезпечити високоякісний ремонт за розумними цінами.
+            </p>
+
+            <h2 className="fs-2 fw-light m-4">
+                Прейскурант
+            </h2>
+
+
         </div>
     );
 }
