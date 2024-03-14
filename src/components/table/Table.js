6a22b30e55a56b904843af2514904ff6b0415991
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2024-03-14 21:52:27 +0200

    fix table for accordion

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -16,7 +16,11 @@ const Table = ({ repairs, field }) => {
                 {repairs.map((repair) => (
                 <tr key={repair.id}>
                     <td>{repair.name}</td>
-                    <td>{repair.price ? `${repair.price} грн` : 'Уточнюйте'}</td>
+                    <td>
+                        {repair.price !== undefined && repair.price !== null ? `${repair.price} грн` :
+                            repair.warranty !== undefined && repair.warranty !== null ? `${repair.warranty} міс` : 
+                            'Уточнюйте'}
+                    </td>
                 </tr>
                 ))}
             </tbody>
