176f10c87b60255ae2c610e044aff8ffacc1a56b
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2024-03-20 21:31:47 +0200

    button update

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import "./Button.scss";
 
-export default function Button({ children, border, color, size }) {
+export default function Button({ children, border, color, size, ...modal }) {
     const buttonStyle = `button ${border ? 'border' : ''} ${size} ${color}-color`;
 
     return (
-        <button className={buttonStyle}>
+        <button className={buttonStyle} {...modal}>
             {children}
         </button>
     );
